385b57e4a9db977e41f3b5f5fa58dc26fad4da8b
Sneha-Jambagi06 <snehajambagi2020@gmail.com>
2025-12-04 17:39:28 +0530

    Update fee.py

--- fee.py
+++ fee.py
@@ -5,7 +5,6 @@ if len(sys.argv)==3:
 else:
     tuition_fees=2000.0
     paid_amount=2000.0
-    balance=2000.0
 total=balance+paid_amount
 print("Tuition_Fees:",tuition_fees)
 print("Paid_amount:",paid_amount)
