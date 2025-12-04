20f1de7b0e968f5f3990b796be7a1a34dc79250c
Sneha-Jambagi06 <snehajambagi2020@gmail.com>
2025-12-04 16:33:09 +0530

    save

---
+++ fee.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.argv)==4:
+    tuition_fees=float(sys.argv[1])
+    paid_amount=float(sys.argv[2])
+    balance=float(sys.argv[3])
+else:
+    tuition_fees=2000.0
+    paid_amount=2000.0
+    balance=2000.0
+total=balance+paid_amount
+print("Tuition_Fees:",tuition_fees)
+print("Paid_amount:",paid_amount)
+print("Balance:",balance)
+print("Total_fees:",total)
